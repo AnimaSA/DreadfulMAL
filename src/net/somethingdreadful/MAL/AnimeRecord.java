d1f0d18c64db65fa3e865583fc11d633905d2885
Chris Fanti <animabolt@gmail.com>
2012-05-23 18:06:01 -0400

    can't remember what I did here, commiting

---
+++ src/net/somethingdreadful/MAL/AnimeRecord.java
@@ -0,0 +1,57 @@
+package net.somethingdreadful.MAL;
+
+public class AnimeRecord {
+	private String ID;
+	private String Name;
+	private String Synopsis;
+	private String Watch_Status;
+	private String Watched_Episodes;
+	private String Anime_Status;
+	private String Total_Episodes;
+	
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getSynopsis() {
+		return Synopsis;
+	}
+	public void setSynopsis(String synopsis) {
+		Synopsis = synopsis;
+	}
+	public String getWatch_Status() {
+		return Watch_Status;
+	}
+	public void setWatch_Status(String watch_Status) {
+		Watch_Status = watch_Status;
+	}
+	public String getWatched_Episodes() {
+		return Watched_Episodes;
+	}
+	public void setWatched_Episodes(String watched_Episodes) {
+		Watched_Episodes = watched_Episodes;
+	}
+	public String getAnime_Status() {
+		return Anime_Status;
+	}
+	public void setAnime_Status(String anime_Status) {
+		Anime_Status = anime_Status;
+	}
+	public String getTotal_Episodes() {
+		return Total_Episodes;
+	}
+	public void setTotal_Episodes(String total_Episodes) {
+		Total_Episodes = total_Episodes;
+	}
+
+	
+
+}
